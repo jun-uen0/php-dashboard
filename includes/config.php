a3214a206f954c61342b78baa32cf5bd48b3e278
jun-uen0 <hi@jaysan.io>
2021-12-01 15:55:47 +0900

    translated into Japanese

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,6 @@ define('DB_PASS' ,'root');
 define('DB_NAME', 'loginsystem');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
-// Check connection
 if (mysqli_connect_errno())
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
